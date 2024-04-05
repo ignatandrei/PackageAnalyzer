21293c7996566463d9bf4e1c4b2a2c456bfd99a9
Andrei Ignat <ignatandrei@yahoo.com>
2024-04-05 10:51:12 +0300

    buidl

---
+++ src/NetPackageAnalyzer/build.ps1
@@ -0,0 +1,13 @@
+while($true)
+{
+
+    cls
+    Write-Host "delete obj and bin"
+    gci obj -recurse | foreach{ri $_.FullName -recurse -force }
+    gci bin -recurse | foreach{ri $_.FullName -recurse -force }
+    dotnet clean
+    dotnet restore
+    dotnet build /p:EmitCompilerGeneratedFiles=true --disable-build-servers --force
+    Read-Host -Prompt "Press Enter to continue"
+
+}
