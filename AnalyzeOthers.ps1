5e760dc0a84889e73088b01065233d13722bc512
Andrei Ignat <ignatandrei@yahoo.com>
2024-06-29 11:56:32 +0300

    Update AnalyzeOthers.ps1

--- AnalyzeOthers.ps1
+++ AnalyzeOthers.ps1
@@ -1,13 +1,43 @@
 ﻿$url=  "https://github.com/fullstackhero/dotnet-starter-kit"
 $url="https://github.com/ivanpaulovich/clean-architecture-manga"
+#many sln
 $url ="https://github.com/evolutionary-architecture/evolutionary-architecture-by-example"
 $url="https://github.com/jasontaylordev/CleanArchitecture"
 $url="https://github.com/jbogard/ContosoUniversityDotNetCore-Pages"
+#3 sln
 $url="https://github.com/dotnet/eShop"
 $url="https://github.com/kgrzybek/modular-monolith-with-ddd"
 
-
 $name = $url.Split('/')[-1]
+$analyzerPath = Get-Location
+$copyPath = Join-Path  $analyzerPath 'src' 
+$copyPath = Join-Path $copyPath 'documentation1'
+$copyPath = Join-Path $copyPath 'docs'
+$copyPath = Join-Path $copyPath 'analysis'
+$copyPath = Join-Path $copyPath $name
+
+
+if(-not (Test-Path $copyPath)) {
+    New-Item -ItemType Directory -Path $copyPath    
+$jsonContent = @"
+{
+    "label": "$name",
+    "position": 1,
+    "link": {
+        "type": "generated-index"
+    }
+}
+"@
+
+# Specify the file path where you want to save the JSON content
+$filePath = Join-Path $copyPath '_category_.json'
+
+# Write the JSON content to the file
+Set-Content -Path $filePath -Value $jsonContent
+
+}
+
+
 Push-Location ..
 
 if(-not (Test-Path $name)) {
@@ -17,8 +47,10 @@ if(-not (Test-Path $name)) {
 cd $name
 $currentPath = Get-Location
 $sln = (Get-ChildItem *.sln -r)
-Write-Host "found $sln.Count solutions"
+Write-Host "found $($sln.Count) solutions"
+$index=0
 $sln | ForEach-Object {
+    $index++
     Write-host $_.FullName 
     $solutionPath = Split-Path $_.FullName
     Set-Location $solutionPath
@@ -32,17 +64,21 @@ $sln | ForEach-Object {
 
     dotnet PackageAnalyzer generateFiles
     cd Analysis
-    npm install
+    cd docs
+    cd Analysis
+    $destination = Join-Path $copyPath $index
+    if(-not (Test-Path $destination)) {
+        New-Item -ItemType Directory -Path $destination    
+    }
+    Copy-Item -Path * -Destination $destination -Recurse
+    #npm install
     #npm run start
-    # Start the npm process and redirect output to null to keep the script running
-    $processInfo = Start-Process npm -ArgumentList "run", "start" -PassThru -NoNewWindow
+    # $processInfo = Start-Process npm -ArgumentList "run", "start" -PassThru -NoNewWindow
     
-    # Wait for 1 minute (60000 milliseconds)
-    Start-Sleep -Milliseconds 30000
+    #Start-Sleep -Milliseconds 30000
     
-    # Stop the process
-    Stop-Process -Id $processInfo.Id
-    Set-Location $currentPath
+    # Stop-Process -Id $processInfo.Id
+    # Set-Location $currentPath
 
 }
 
