3f33ca0c5498c9a7f9973bb1d72e35ba15ad3815
Andrei Ignat <ignatandrei@yahoo.com>
2024-07-05 20:00:34 +0300

    generating files

--- AnalyzeMe.ps1
+++ AnalyzeMe.ps1
@@ -5,17 +5,36 @@ Remove-Item
 Get-ChildItem -Path "docs"  -Recurse |
 Where-Object { $_.Name -ne "ico.png"} |
 Remove-Item -Recurse
+
+
+
 $currentPath = Get-Location
-$destination = Join-Path $currentPath "docs"
+
+$docs = Join-Path $currentPath "docs"
+
+$destination = Join-Path $currentPath "src"
+$destination = Join-Path $destination "documentation1"
+
+Remove-Item $destination -Recurse
+
+
 Push-Location .
 Set-Location -Path "src"
 Set-Location -Path "NetPackageAnalyzer"
 dotnet build 
-dotnet new tool-manifest
+# dotnet new tool-manifest
 dotnet tool uninstall netpackageanalyzerconsole
 dotnet tool update netpackageanalyzerconsole
 Write-Host "Current path: $(Get-Location)"
 
 dotnet PackageAnalyzer generateFiles --where $destination
+
+Set-Location $destination
+
+npm i
+npm run build
+$destination = Join-Path $destination "build"
+Set-Location $destination
+Copy-Item -Path .\* -Destination $docs -Recurse 
 
 Pop-Location
---
+++ docs/assets/js/9278.ba2e930b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[9278],{9278:(e,n,t)=>{t.r(n)}}]);
