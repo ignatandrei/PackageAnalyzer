ca3733f64c5219cd96ad3599c03f17caa88cfa25
Andrei Ignat <ignatandrei@yahoo.com>
2024-09-04 03:24:15 -0700

    v8.2024.904.323

--- src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
@@ -29,20 +29,20 @@
         //    "--folder", @"D:\gth\ContosoUniversityDotNetCore-Pages",
         //    "--verbose","false"
         //};
- 
+
         //args = new[] { "generateFiles",
         //    "--folder", @"D:\gth\eShop",
         //    "-wg","HtmlSummary",
         //    "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
         //    "--verbose","false"
         //};
-         
-        args = new[] { "generateFiles",
-            "--folder", @"D:\gth\RSCG_Examples\v2",
-            "-wg","HtmlSummary",
-            "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
-            "--verbose","false"
-        };
+
+        //args = new[] { "generateFiles",
+        //    "--folder", @"D:\gth\RSCG_Examples\v2",
+        //    //"-wg","HtmlSummary",
+        //    "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
+        //    "--verbose","false"
+        //};
 
 
     }
