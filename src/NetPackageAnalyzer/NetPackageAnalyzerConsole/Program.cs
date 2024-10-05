68cc07db65c878c301ea1249f6b8737ea03aa295
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-05 16:12:38 +0300

    more remove compiling errors

--- src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
@@ -51,7 +51,7 @@
             "-wg","HtmlSummary",
             "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
             "--verbose","false"
-        };
+        }; 
         //args = new[] { "generateFiles",
         //    "--folder", @"D:\source\test\NetPackageAnalyzer",
         //    "-wg","HtmlSummary",
