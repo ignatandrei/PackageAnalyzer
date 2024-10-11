a8eb9819c6aaca0382fb1bba25dc8bee0312be4b
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-11 18:41:54 +0300

    more tests

--- src/NetPackageAnalyzer/NetPackageAnalyzerTests/TestStatistics.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerTests/TestStatistics.cs
@@ -21,5 +21,28 @@ public class TestStatistics
         Assert.AreEqual(261, median);
     }
 
+    [TestMethod]
+    public void TestMode()
+    {
+        var values = new int[] { 5, 5,100,100,100, 300, 400, 500 };
+        var mode = StatisticalNumbers<int>.Mode(values);
+        var res= new int[] { 100 };
+        Assert.AreEqual(3, mode.Item2);
+        Assert.AreEqual(res.Length, mode.Item1.Length);
+        for (int i = 0; i < res.Length; i++)
+        {
+            Assert.AreEqual(res[i], mode.Item1[i]);
+        }
+        values = new int[] { 5, 5, 5, 100, 100, 100, 300, 400, 500 };
+        mode = StatisticalNumbers<int>.Mode(values);
+        res = new int[] { 5,100 };
+        Assert.AreEqual(3, mode.Item2);
+        Assert.AreEqual(res.Length, mode.Item1.Length);
+        for (int i = 0; i < res.Length; i++)
+        {
+            Assert.AreEqual(res[i], mode.Item1[i]);
+        }
+    }
+
 
 }
