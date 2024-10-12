8d184c52d2c224b585bb40add99924dc05c0e9eb
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-12 06:53:29 +0300

    started statistcs

--- src/NetPackageAnalyzer/NetPackageAnalyzerObjects/many.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerObjects/many.cs
@@ -44,7 +44,7 @@ public record NamePerCount(string Name, long Count)
 }
 public record NamePerCountArray(NamePerCount[]? NamePerCounts,bool Descending)
 { 
-    public Statistics<long> Data()
+    public Statistics<long> Statistics()
     {
         return new Statistics<long>(NamePerCounts?.Select(it => it.Count).ToArray() ?? []);
     }
