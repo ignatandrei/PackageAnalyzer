ad1c63a1b9997dfca63458a46d7eb41a181731a7
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-19 10:36:04 +0300

    project name

--- src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
@@ -86,7 +86,7 @@
     return await RealMainExecuting.RealMain(args);
 } 
 catch (Exception ex) 
-{
+{ 
     Console.WriteLine(ex.Message);
     //WhatIAmDoingData.DisplayData.DisplayJustErrors();
     return 1;
--- src/NetPackageAnalyzer/NetPackageAnalyzerObjects/generatedPartial/IOperations.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerObjects/generatedPartial/IOperations.cs
@@ -8,34 +8,65 @@ public interface IOperations
     Dictionary<string, PackageWithVersion[]> PerProjectPathWithVersion();
 
 }
+public class WhyData
+{
+    public string ProjectText { get; set; } = string.Empty;
+    public string ProjectName()
+    {
+        var last = "has the following";
+        var first = "Project";
+        var start = ProjectText.IndexOf(last);
+        var text = ProjectText.Substring(ProjectText.IndexOf(last));
+        text=text.Replace(first, string.Empty);
+        text=text.Replace("'", string.Empty);
+        text=text.Trim();
+        return text;
+    }
+    public string WhyText { get; set; } = string.Empty;
+}
 public record PackageWithVersion(string PackageId,string RequestedVersion, PackageOptions PackageOptions = PackageOptions.None)
 {
     public ProjectData[] Projects { get; set; } = Array.Empty<ProjectData>();
     public void VerifyWhy()
     {
         if(Why.Length>0)return;
-
+        List<WhyData> whyDatas = new();
         ProcessOutput processOutput = new();
         try
         {
 
             var str = processOutput.OutputWhy(GlobalsForGenerating.FullPathToSolution, PackageId);
             var arrStr = str.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); ;
-            foreach(var item in arrStr)
+            WhyData? whyData = null;
+            foreach (var item in arrStr)
             {
-                if (!item.Contains("does not have a dependency on"))
+                if (item.Contains("does not have a dependency on"))
+                    continue;
+                if(string.IsNullOrWhiteSpace(item?.Trim()))
                 {
-                    Why += item +Environment.NewLine;                    
+                    continue;
                 }
+                if (item.Contains("has the following dependency"))
+                {
+                    if(whyData != null)
+                    {
+                        whyDatas.Add(whyData);
+                    }
+                    whyData = new ();
+                    whyData.ProjectText = item;
+                    continue;
+                }
+                whyData!.WhyText += item;
             }
         }
         catch (Exception ex)
         {
-            Why = ex.Message;
+            Console.WriteLine($"!!!Error in  dependency {PackageId} : "+ ex.Message);
         }
+        Why = whyDatas.ToArray();
     }
 
-    public string Why=string.Empty;
+    public WhyData[] Why =[];
 
 
 }
