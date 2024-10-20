5acdadd1c18125a2e1d27cb9d3bfc5f186309168
Andrei Ignat <ignatandrei@yahoo.com>
2024-10-20 04:59:14 +0300

    v8.2024.1020.458

--- src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
@@ -64,12 +64,12 @@
         //    "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
         //    "--verbose","false"
         //};
-        args = new[] { "generateFiles",
-            "--folder", @"D:\gth\TILT\src\backend\Net6\NetTilt",
-            "-wg","HtmlSummary",
-            "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
-            "--verbose","false"
-        };
+        //args = new[] { "generateFiles",
+        //    "--folder", @"D:\gth\TILT\src\backend\Net6\NetTilt",
+        //    "-wg","HtmlSummary",
+        //    "--where", @"D:\gth\PackageAnalyzer\src\documentation1",
+        //    "--verbose","false"
+        //};
         //args = new[] { "generateFiles",
         //    "--folder", @"D:\gth\RSCG_Examples\v2",
         //    "-wg","HtmlSummary",
