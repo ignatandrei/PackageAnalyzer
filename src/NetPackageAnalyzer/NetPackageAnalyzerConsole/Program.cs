3f0e560d7d0ca8d270e5eb2fe734695c20e5cb26
Andrei Ignat <ignatandrei@yahoo.com>
2025-12-17 20:52:41 +0200

    VERSION

--- src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
+++ src/NetPackageAnalyzer/NetPackageAnalyzerConsole/Program.cs
@@ -5,12 +5,12 @@
     if (args.Length == 0)
     {
         args = new[] { "-h" };
-        args = new[] { "generateFiles",
-            "--folder", @"D:\eu\GitHub\XpertContract-\src\XpertContract",
-            "-wg","HtmlSummary",
-            "--where", @"D:\eu\GitHub\PackageAnalyzer\src\documentation1\",
-            "--verbose","true"
-        };
+        //args = new[] { "generateFiles",
+        //    "--folder", @"D:\eu\GitHub\XpertContract-\src\XpertContract",
+        //    "-wg","HtmlSummary",
+        //    "--where", @"D:\eu\GitHub\PackageAnalyzer\src\documentation1\",
+        //    "--verbose","true"
+        //};
 
         // args = new[] { "generateFiles",
         //    "--folder", @"D:\gth\CleanArchitecture",
